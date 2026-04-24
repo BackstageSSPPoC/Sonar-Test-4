3298a943a20ebf6e46fb1c3c5c3fdc786a2b1f54
Chaitanya Shekhar Pandey <113713770+Chaitanya-Shekhar-Pandey@users.noreply.github.com>
2026-04-24 16:48:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end(`
     <body style="background:white;font-family:Arial;text-align:center;margin-top:40px;">
-      <h1 style="color:blue;">Welcome to GSPANN Self Service Portal!</h1>
+      <h1 style="color:blue;">Welcome to DevOps Self Service Portal!</h1>
       <p style="color:gray;font-size:22px;">
-        Drive Faster Releases, Better Quality, and Business Growth with CI/CD Automation in Developers Existing GitHub Repositories 🚀
+        DSSP
       </p>
     </body>
   `);
